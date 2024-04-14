6bae1d8ac17648ba0bc5bae49a458299dade1e1d
Nikita <kniki098@gmail.com>
2024-04-14 22:32:29 +0530

    Commit changes

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIR = {
 }
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
